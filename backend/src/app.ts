31736ce5a921db1add3dc90d5012dc7cf3b5dc9d
Avi1305 <avinash13052001@gmail.com>
2026-04-10 16:24:14 +0530

    refactor: clean up CORS configuration formatting for better readability

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,23 +4,24 @@ import analyzeRoutes from "./routes/analyze.route.js";
 
 const app = express();
 
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://ai-codebase-analyzer-iota.vercel.app"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}));
-
-
-
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://ai-codebase-analyzer-iota.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 
 app.use("/api", analyzeRoutes);
 
-
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
 export default app;
